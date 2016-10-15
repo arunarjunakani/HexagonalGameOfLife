f16a9f859c02b23d5f514a2c654d5f7a4557c19b
Arun Arjunakani <arunarjunakani@gmail.com>
2016-10-15 16:47:37 -0400

    Updated src.js
    Changed the grid so that it has the same number of rows and columns.
    Also added the option to create a size 3 (100 x 100) grid

--- src.js
+++ src.js
@@ -92,73 +92,52 @@ function populateArray(size)
 {
   //This clears the array so this method can be run several times.
   grid = [];
-
-
   if(size == 1)
   {
     for(var i = 0; i < 5; i++)
     {
       //The individual rows will be added one at a time
       var row = [];
-      if(i%2 == 0)
+      for(var j = 0; j < 5; j++)
       {
-        //If the row is even numbered, this will make 4 nodes in the row. Otherwise, it will make 5
-        for(var j = 0; j < 4; j++)
-        {
-          //Adds a new node to the row
-          var hex = new Node(i, j);
-          row.push(hex);
-          console.log("Even Row");
-        }
-      }
-      else
-      {
-        for(var j = 0; j < 5; j++)
-        {
-          //Adds a new node to the row
-          var hex = new Node(i, j);
-          row.push(hex);
-          console.log("Odd Row");
-        }
+        //Adds a new node to the row
+        var hex = new Node(i, j);
+        row.push(hex);
       }
       grid.push(row);
     }
   }
   else if(size == 2)
   {
-    for(var i = 0; i < 19; i++)
+    for(var i = 0; i < 20; i++)
     {
       //The individual rows will be added one at a time
       var row = [];
-      if(i%2 == 0)
+      for(var j = 0; j < 20; j++)
       {
-        //If the row is even numbered, this will make 4 nodes in the row. Otherwise, it will make 5
-        for(var j = 0; j < 20; j++)
-        {
-          //Adds a new node to the row
-          var hex = new Node(i, j);
-          row.push(hex);
-          console.log("Even Row");
-        }
-      }
-      else
-      {
-        for(var j = 0; j < 21; j++)
-        {
-          //Adds a new node to the row
-          var hex = new Node(i, j);
-          row.push(hex);
-          console.log("Odd Row");
-        }
+        //Adds a new node to the row
+        var hex = new Node(i, j);
+        row.push(hex);
       }
       grid.push(row);
     }
   }
-  /*
-  else()
-  {
 
-  }*/
+  else
+  {
+    for(var i = 0; i < 100; i++)
+    {
+      //The individual rows will be added one at a time
+      var row = [];
+      for(var j = 0; j < 100; j++)
+      {
+        //Adds a new node to the row
+        var hex = new Node(i, j);
+        row.push(hex);
+      }
+      grid.push(row);
+    }
+  }
   console.log(grid);
 }
 
@@ -192,21 +171,6 @@ function getNeighbors(i, j)
     if((node.n)%2 == 0)
     {
       console.log("Even row");
-      if(j+1 < grid[1].length)
-      {
-        if(i-1 >= 0)
-        {
-          neighbors.push(grid[i-1][j+1]);
-        }
-        if(i+1 < grid.length)
-        {
-          neighbors.push(grid[i+1][j+1]);
-        }
-      }
-    }
-    else
-    {
-      console.log("Odd row");
       if(j-1 >= 0)
       {
         if(i-1 >= 0)
@@ -216,6 +180,21 @@ function getNeighbors(i, j)
         if(i+1 < grid.length)
         {
           neighbors.push(grid[i+1][j-1]);
+        }
+      }
+    }
+    else
+    {
+      console.log("Odd row");
+      if(j+1 < grid[i].length)
+      {
+        if(i-1 >= 0)
+        {
+          neighbors.push(grid[i-1][j+1]);
+        }
+        if(i+1 < grid.length)
+        {
+          neighbors.push(grid[i+1][j+1]);
         }
       }
     }
